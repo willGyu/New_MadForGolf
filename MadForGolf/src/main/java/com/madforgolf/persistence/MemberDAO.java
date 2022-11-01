edf9ca535e06f69b2802cd9ca44c518a6544ebeb
ITWILL <ITWILL@DESKTOP-NKRQSUI>
2022-11-01 11:07:48 +0900

    기능 수정 : 로그인, 회원정보수정, 메인페이지
    로그인 수정 완료, 회원정보수정 작업중, 메인페이지 CSS 수정중

--- MadForGolf/src/main/java/com/madforgolf/persistence/MemberDAO.java
+++ MadForGolf/src/main/java/com/madforgolf/persistence/MemberDAO.java
@@ -15,25 +15,38 @@ public interface MemberDAO {
 		//아이디 중복 체크 
 		public int idCheck(MemberVO vo) throws Exception;
 		
+		//휴대번호 중복 체크   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    1029 서하 추가
+		public int phoneCheck(MemberVO vo) throws Exception;
+		
 		// 로그인 동작
 		public MemberVO login(MemberVO vo) throws Exception;
 		
 		// 로그인 동작2 (메서드 오버로딩)
 		public MemberVO login(String user_id, String user_pw) throws Exception;
 		
-				
+		
+		// 아이디 찾기
+		public MemberVO findId(MemberVO vo) throws Exception;
+		
+		// 비밀번호 찾기
+//		public MemberVO findPw(MemberVO vo) throws Exception;
+		
+		// 비밀번호 변경
+		public MemberVO updatePw(MemberVO vo) throws Exception;
+		
+		
 		// 회원정보 조회
 		public MemberVO getMember(String id);
 		
 		// 회원정보 수정
-		public Integer updateMember(MemberVO uvo);
+//		public Integer updateMember(MemberVO uvo);
 		
-		// 회원정보 삭제 .
-		public Integer deleteMember(MemberVO dvo);
+		// 회원 탈퇴
+//		public void deleteMember(MemberVO dvo) throws Exception;
 		
 		// 회원목록 리스트 조회
 		public List<MemberVO> getMemberList();
-	
+		
 	
 }
 
--- MadForGolf/src/main/java/com/madforgolf/service/MemberServiceImpl.java
+++ MadForGolf/src/main/java/com/madforgolf/service/MemberServiceImpl.java
@@ -14,7 +14,7 @@ import com.madforgolf.persistence.MemberDAO;
 @Service
 public class MemberServiceImpl implements MemberService{
 
-	private static final Logger logger 
+	private static final Logger log
 			= LoggerFactory.getLogger(MemberServiceImpl.class);
 
 
@@ -25,13 +25,12 @@ public class MemberServiceImpl implements MemberService{
 		this.dao= dao;
 	}
 	
-	//회원 가입 처리
-
+	//회원 가입
 	@Override
 	public void insert(MemberVO vo) throws Exception {
-		logger.info("MemberServiceImpl - insert() 호출");
+		log.info("MemberServiceImpl - insert() 호출");
 		dao.insert(vo);
-		logger.info("DAO 동작 완료! 서비스 -> 컨트롤러");
+		log.info("DAO 동작 완료! 서비스 -> 컨트롤러");
 
 	}
 	//아이디 중복 체크
@@ -39,12 +38,20 @@ public class MemberServiceImpl implements MemberService{
 	public int idCheck(MemberVO vo) throws Exception {
 		return dao.idCheck(vo);
 	}
-
+	
+	//휴대번호 중복 체크  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    1029 서하 추가
+	@Override
+	public int phoneCheck(MemberVO vo) throws Exception {
+		return dao.phoneCheck(vo);
+	}
+	
+	
+	
 	// 로그인
 	@Override
 	public MemberVO login(MemberVO vo) throws Exception {
 
-	logger.info("MemberServiceImpl - login(MemberVO vo)  호출");
+	log.info("MemberServiceImpl - login(MemberVO vo)  호출");
 	
 	return dao.login(vo);
 	}
@@ -56,32 +63,76 @@ public class MemberServiceImpl implements MemberService{
 	}
 	
 	
+	
+	
+	
+	
 	@Override
 	public MemberVO getMember(String id) {
 		//메서드 호출
 		return dao.getMember(id);
 	}
-
+//	
+//	@Override
+//	public Integer updateMember(MemberVO uvo) {
+//		//메서드 호출
+//		return dao.updateMember(uvo);
+//	}
+//
+//	@Override
+//	public MemberVO loginMember(MemberVO vo) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	@Override
+//	public MemberVO loginMember(String userid, String userpw) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+	
+	
+	
+	
+	
+	
+	
+	
+	// 아이디 찾기
 	@Override
-	public Integer updateMember(MemberVO uvo) {
-		//메서드 호출
-		return dao.updateMember(uvo);
+	public MemberVO findId(MemberVO vo) throws Exception {
+		log.info("findId(MemberVO vo) 호출");
+		
+		return dao.findId(vo);
+	}
+	
+	// 비밀번호 찾기
+//	@Override
+//	public MemberVO findPw(MemberVO vo) throws Exception {
+//		log.info("findPw(MemberVO vo) 호출");
+//		
+//		return dao.findPw(vo);
+//	}
+	
+	// 비밀번호 변경
+	@Override
+	public MemberVO updatePw(MemberVO vo) throws Exception {
+		log.info("updatePw(MemberVO vo)");
+		
+		log.info(" serviceImpl @@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+vo);
+		return dao.updatePw(vo);
 	}
 
-	@Override
-	public MemberVO loginMember(MemberVO vo) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public MemberVO loginMember(String userid, String userpw) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-
+	// 회원 탈퇴
+//	@Override
+//	public void deleteMember(String user_id) throws Exception {
+//		log.info("deleteMember(MemberVO dvo) 호출");
+//		
+//		log.info("@@@@@@@@@@@ serviceImpl"+user_id);
+//		
+//		return dao.deleteMember(user_id);
+//	}
 }
 
 	

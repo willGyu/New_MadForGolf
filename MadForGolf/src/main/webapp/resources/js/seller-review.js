0c8beca7592abaf96bd0186df5ab745c5e25c815
devEine <112680268+devEine@users.noreply.github.com>
2022-11-17 15:33:55 +0900

    기능추가: 셀러 정보페이지 css수정

--- MadForGolf/src/main/webapp/resources/js/seller-review.js
+++ MadForGolf/src/main/webapp/resources/js/seller-review.js
@@ -159,14 +159,7 @@ const sellerReview={
                                             <img src="/resources/product_img/${item.prod_img}" alt="">
                                             </a>
 	                                <!-- Product Tag -->
-	                                <div class="product-tag">
-	                                    <a href="#">Hot</a>
-	                                </div>
-	                                <div class="product-meta d-flex">
-	                                    <a href="#" class="wishlist-btn"><i class="icon_heart_alt"></i></a>
-	                                    <a href="cart.html" class="add-to-cart-btn">Add to cart</a>
-	                                    <a href="#" class="compare-btn"><i class="arrow_left-right_alt"></i></a>
-	                                </div>
+	                              
 	                            </div>
 	                            <!-- Product Info -->
 	                            <div class="product-info mt-15 text-center">
